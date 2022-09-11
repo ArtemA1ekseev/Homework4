273abe06265f65ef1c9a0f3925536d86e3edc522
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-11 19:19:25 +0400

    Задача 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Задача 1.");
+        int Age = 21;
+        if (Age >= 18) {
+            System.out.println("Поздравляю с совершеннолетием!");
+        }
+        if (Age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать!");
+        }
     }
 }
