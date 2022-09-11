ff9b0fccc3bf2dcb1fc9c17958cd878f07bc3913
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-11 23:05:02 +0400

    Домашнее задание - 3. Задача 9.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 8.");
-        int Age2 = 10;
-        if (Age2 < 5) {
-            System.out.println("Ребенок не может кататься на аттракционе.");
-        } else if (Age2 < 14) {
-            System.out.println("Ребенок может кататься только в сопровождении взрослого.");
+        System.out.println("Задача 9.");
+        int one = 131;
+        int two = -342;
+        int three = 5;
+        if (one > two) {
+            if (one >= three) {
+                System.out.println("Максимальное число: " + one);
+            } else {
+                System.out.println("Максимальное число: " + three);
+            }
+        } else if (two > one) {
+            if (two >= three) {
+                System.out.println("Максимальное число: " + two);
+            } else {
+                System.out.println("Максимальное число: " + three);
+            }
         } else {
-            System.out.println("Ребенок может кататься без сопровождения взрослого.");
+            if (one > three) {
+                System.out.println("Максимальное число: " + one);
+            } else if (three > one) {
+                System.out.println("Максимальное число: " + three);
+            } else {
+                System.out.println("Числа равны");
+            }
         }
     }
 }
